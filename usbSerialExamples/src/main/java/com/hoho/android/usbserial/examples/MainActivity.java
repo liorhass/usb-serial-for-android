9984368f660aece7ca2fa774c174011231d8f953
Lior Hass <lior.hass@gmail.com>
2020-11-26 07:34:13 +0200

    A few IDE-related changes made while developping LoraChat.

--- usbSerialExamples/src/main/java/com/hoho/android/usbserial/examples/MainActivity.java
+++ usbSerialExamples/src/main/java/com/hoho/android/usbserial/examples/MainActivity.java
@@ -1,3 +1,15 @@
+// ADB connect to a device over WiFi:   https://developer.android.com/studio/command-line/adb#wireless
+//   Connect the device to the host computer with a USB cable.
+//        adb tcpip 5555
+//   Find the IP address of the device: Settings -> System -> About tablet -> Status
+//        adb connect <device_ip_address>
+//        adb devices   (to verify)
+//   If the adb connection is ever lost:
+//     Reconnect by executing the    adb connect <device_ip_address>    step again.
+//     If that doesn't work, reset your adb host:
+//        adb kill-server
+//     Then start over from the beginning.
+//
 package com.hoho.android.usbserial.examples;
 
 import android.content.Intent;
